da772cc68692d02eb4a20417bc9cec6e366d1ba0
Yudo Kartiko <yudoworks@gmail.com>
2021-11-15 21:17:02 +0700

    fix type on routes.rb resource to resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :users do
-        resource :facts
+      resources :users do
+        resources :facts
       end
     end
   end
