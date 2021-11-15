b70bae5b6513bbd52b5e6afbb24d4c6f914c1715
Yudo Kartiko <yudoworks@gmail.com>
2021-11-15 21:14:10 +0700

    create CRUD fact controller

--- app/controllers/api/v1/facts_controller.rb
+++ app/controllers/api/v1/facts_controller.rb
@@ -3,6 +3,45 @@
 module Api
   module V1
     class FactsController < ApplicationController
+      before_action :find_fact, only: %i[show update destroy]
+
+      def index
+        @facts = Fact.all
+        render json: @facts
+      end
+
+      def show
+        render json: @fact
+      end
+
+      def create
+        @fact = Fact.new(fact_params)
+
+        if @fact.save
+          render json: @fact
+        else
+          render error: { error: 'Unable to create fact' }, status: 400
+        end
+      end
+
+      def destroy
+        if @fact
+          @fact.destroy
+          render json: { message: 'Fact successfully deleted' }, status: 200
+        else
+          render error: { error: 'Unable to delete fact' }, status: 400
+        end
+      end
+
+      private
+
+      def fact_params
+        params.require(:fact).permit(:fact, :like, :user_id)
+      end
+
+      def find_fact
+        @fact = Fact.find(params[id])
+      end
     end
   end
 end
